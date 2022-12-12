136564917fc8eda78453317d8344f3781147f4c2
Mazyro <amazyrko@gmail.com>
2022-12-13 01:27:42 +0200

    Add HT4

---
+++ home_task_3.py
@@ -0,0 +1,47 @@
+import copy
+
+# 1. Создать 3 переменные одного и тоже типа с одинаковыми
+# данными и с одинаковыми идентификаторами.
+print(1, 50*"-")
+var1 = 'test'
+var2 = 'test'
+var3 = 'test'
+
+# print("var1: ", var1, "Type :", type(var1), "ID: ", id(var1))
+# print("var2: ", var2, "Type :", type(var2), "ID: ", id(var2))
+# print("var3: ", var3, "Type :", type(var3), "ID: ", id(var3))
+print("Equal: ", var1 == var2 == var3)
+print("Equal ID: ", var1 is var2 is var3)
+
+# 2. Создать 2 переменные одного и тоже типа с одинаковыми
+# данными и с разными идентификаторами.
+print(2, 50*"-")
+var4 = [1, 2, ('a', 'b', 'c'), 4]
+var5 = [1, 2, ('a', 'b', 'c'), 4]
+
+# print("var4: ", var4, "Type :", type(var4), "ID: ", id(var4))
+# print("var5: ", var5, "Type :", type(var5), "ID: ", id(var5))
+print("Equal: ", var5 == var4)
+print("Equal ID: ", var4 is var5)
+
+# 3*. Поменять их типы так, чтобы у 1-х трёх стали разные идентификаторы,
+# но при этом остались одинаковые данные
+# (и одинаковый тип), а у 2-х последних стали одинаковые идентификаторы
+# и остаhome_task_3лись одинаковые данные (и одинаковый тип).
+# добиться нужного результата необходимо только приведением типов данных к нужному
+
+print(3, 50*"-")
+var1 = list(var1)
+var2 = list(var2)
+var3 = list(var3)
+print("Equal: ", var1 == var2 == var3)
+print("Equal ID: ", var1 is var2 is var3)
+print(20*"-")
+
+var4 = tuple(var4)
+var5 = copy.copy(var4)
+print("var4, ", id(var4))
+print("var5, ", id(var5))
+print("Equal: ", var5 == var4)
+print("Equal ID: ", var4 is var5)
+print("END", 50*"-")
---
+++ home_task_4.py
@@ -0,0 +1,65 @@
+# визначення більшого числа з двох
+# (функція приймає 2 числа та на виході повертає більше);
+def number_compare_biggest(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+print("biggest number among two is - ", number_compare_biggest(150, -150))
+
+
+# визначення меншого числа з трьох
+# (аналогічно до попередньої задачі);
+
+def number_compare_smallest(x, y, z):
+    if (x < y) and (x < z):
+        return x
+    elif (y < x) and (y < z):
+        return y
+    else:
+        return z
+
+
+print("smallest number among tree is - ", number_compare_smallest(88, 0, -1))
+
+
+# модуль числа (функція приймає одне значення та повертає його модуль);
+
+def num_module(z):
+    return abs(z)
+
+
+print("module is ", num_module(-5))
+
+# виведення на екран суми значень (функція приймає
+# 2 значення та лише виводить їхню суму на екран);
+
+
+def my_func_suma(x, y):
+    suma = x + y
+    return suma
+
+
+print("Sum equal to ", my_func_suma(5, 5))
+
+# виведення на екран словом чи є число позитивним
+# (функція приймає 1 значення та виводить на екран
+# інформацію про його знак.
+# Особливу увагу варто приділити нульовому значенню).
+
+
+def is_positive(x):
+
+    if (type(x) == int or type(x) == float) and x < 0:
+        return "-"
+    elif (type(x) == int or type(x) == float) and x > 0:
+        return "+"
+    elif (type(x) == int or type(x) == float) and x == 0:
+        return "без знаку"
+    else:
+        return "Pls, pass number format"
+
+
+print("polyarity of passed number is: ", is_positive(0))
